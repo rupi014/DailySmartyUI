d5590bcdee89d73169bf621996c6f60f8e613e3c
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-09-01 11:13:52 +0200

    use axios to perfom get request and update reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-      //perform our request in here.
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log(response.data);
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data
+            })
+        }); 
     }
   }
---
+++ src/reducers/posts-reducers.js
@@ -0,0 +1,21 @@
+import { 
+    SET_RECENT_POSTS 
+} from '../actions/types';
+import recentPost from '../components/recent-post';
+
+const INIT_STATE = {
+    posts: [],
+    recentPosts: []
+}
+
+export default function(state = INIT_STATE, action) {
+    switch(action.type) {
+        case SET_RECENT_POSTS:
+            return {
+                ...state,
+                recentPosts: action.payload
+            }
+        default:
+            return state;
+    }
+}
